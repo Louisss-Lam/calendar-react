de5ccc6e46f2c2f0e639c05e4ae3bf8e14dfd868
louis <louislam617@gmail.com>
2022-07-26 21:10:14 +0100

    feat(calendar): create two-dimensional array for calendar

---
+++ src/components/Month.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Month({month}) {
+  return (
+    <div className="flex-1 grid grid-cols-7 grid-rows-5">
+        {month.map((row, i) => (
+            <React.Fragment key={i}>
+
+            </React.Fragment>
+        ))}
+    </div>
+  )
+}
