5a1bd00a8439b2126cdb9704803846744ffe5e36
louis <louislam617@gmail.com>
2022-08-02 15:34:27 +0100

    feat: add a save button

--- src/components/EventModel.js
+++ src/components/EventModel.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from 'react';
 import GlobalContext from '../context/GlobalContext';
 
+const labelsClasses = ["indigo", "gray", "green", "blue", "red", "purple"];
+
 export default function EventModel() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [selectedLabel, setSelectedLabel] = useState(labelsClasses[0]);
     const {setShowEventModel, daySelected} = useContext(GlobalContext);
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
@@ -46,8 +49,29 @@ export default function EventModel() {
                         className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
                         onChange={(e) => setDescription(e.target.value)}        
                     />
+                    <span className="material-icons-outlined text-gray-400">
+                        bookmark_border
+                    </span>
+                    <div className="flex gap-x-2">
+                        {labelsClasses.map((lblClass, i) => (
+                            <span 
+                                key={i}
+                                onClick={() => setSelectedLabel(lblClass)}
+                                className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
+                            >   
+                                {selectedLabel === lblClass && <span className="material-icons-outlined text-white text-sm">
+                                    check
+                                </span>}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </div>
+            <footer className="flex justify-end w-100 border-t p-3 mt-5">
+                <button type="submit" className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white">
+                            Save
+                </button>
+            </footer>
         </form>
     </div>
   )
