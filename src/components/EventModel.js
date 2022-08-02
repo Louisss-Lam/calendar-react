d8dee8bbbace1d48612ab4b30c16609ab02c55b0
louis <louislam617@gmail.com>
2022-08-02 11:46:50 +0100

    feat: add a description function

--- src/components/EventModel.js
+++ src/components/EventModel.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import GlobalContext from '../context/GlobalContext';
 
 export default function EventModel() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const {setShowEventModel, daySelected} = useContext(GlobalContext);
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
         <form className="bg-white rounded-lg shadow-2xl w-1/4">
@@ -8,12 +12,42 @@ export default function EventModel() {
                 <span className="material-icons-outlined text-gray-400">
                     drag_handle
                 </span>
-                <button>
+                <button onClick={() => setShowEventModel(false)}>
                     <span className="material-icons-outlined text-gray-400">
                         close
                     </span>
                 </button>
             </header>
+            <div className="p-3">
+                <div className="grid grid-cols-1/5 items-end gap-y-7">
+                    <div></div>
+                    <input 
+                        type="text"
+                        name="title" 
+                        placeholder="Add title" 
+                        value={title}
+                        required
+                        className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+                        onChange={(e) => setTitle(e.target.value)}        
+                    />
+                    <span className="material-icons-outlined text-gray-400">
+                        schedule
+                    </span>
+                    <p>{daySelected.format("dddd, MMMM DD")}</p>
+                    <span className="material-icons-outlined text-gray-400">
+                        segment
+                    </span>
+                    <input 
+                        type="text"
+                        name="description" 
+                        placeholder="Add a description" 
+                        value={description}
+                        required
+                        className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+                        onChange={(e) => setDescription(e.target.value)}        
+                    />
+                </div>
+            </div>
         </form>
     </div>
   )
