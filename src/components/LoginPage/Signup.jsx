5719c0c30f9a1559757be232e050f0260319b497
louis <louislam617@gmail.com>
2022-08-29 15:25:40 +0100

    feat: create Signup page

--- src/components/LoginPage/Signup.jsx
+++ src/components/LoginPage/Signup.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div>
-        <h1 className='text-center text-3xl font-bold'>
-            Firebase Auth & Context
-        </h1>
+    <div className='max-w-[700px] mx-auto my-16 p-4'>
+        <h1 className='text-center text-4xl font-bold md:p-9'>Your calendar</h1>
+        <div>
+            <h1 className='text-2xl font-bold py-2'>
+                Sign in to your account
+            </h1>
+            <p clsaaNme='py-2'>
+                Already have an account yet? <Link to='/' className='underline'>Sign in.</Link>
+            </p>
+        </div>
+        <form>
+            <div className='flex flex-col py-2'>
+                <label className='py-2 font-medium'>Email Address</label>
+                <input className='border p-3' type="email"/>
+            </div>
+            <div className='flex flex-col py-2'>
+                <label className='py-2 font-medium'>Password</label>
+                <input className='border p-3' type="password" />
+            </div>
+            <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>Sign Up</button>
+        </form>
     </div>
   )
 }
