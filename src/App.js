bd358c9c6404def0b9680d00009902f94d7fffc7
louis <louislam617@gmail.com>
2022-08-29 11:46:15 +0100

    feat: move calender to account page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { getMonth } from './util';
-import CalendarHeader from './components/CalendarHeader';
-import Sidebar from './components/Sidebar';
-import Month from './components/Month';
-import GlobalContext from './context/GlobalContext';
-import EventModel from './components/EventModel';
+// import { getMonth } from './util';
+// import CalendarHeader from './components/CalendarHeader';
+// import Sidebar from './components/Sidebar';
+// import Month from './components/Month';
+// import GlobalContext from './context/GlobalContext';
+// import EventModel from './components/EventModel';
+import Signup from './components/LoginPage/Signup';
+import Signin from './components/LoginPage/Signin';
+import Account from './components/LoginPage/Account';
+
 
 function App() {
-  const [currentMonth, setCurrentMonth] = useState(getMonth());
-  const { monthIndex, showEventModel } = useContext(GlobalContext);
+  // const [currentMonth, setCurrentMonth] = useState(getMonth());
+  // const { monthIndex, showEventModel } = useContext(GlobalContext);
 
-  useEffect(() => {
-    setCurrentMonth(getMonth(monthIndex));
-  }, [monthIndex])
+  // useEffect(() => {
+  //   setCurrentMonth(getMonth(monthIndex));
+  // }, [monthIndex])
 
   return (
-    <React.Fragment>
-    {showEventModel && <EventModel />}
+    // <React.Fragment>
+    // {showEventModel && <EventModel />}
+    //   <div className="h-screen flex flex-col">
+    //     <CalendarHeader />
+    //     <div className="flex flex-1">
+    //       <Sidebar />
+    //       <Month month={currentMonth}/>
+    //     </div>
+    //   </div>
+    // </React.Fragment>
+    <Routes>
+      <Route path='/' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/account' element={<Account />} />
+    </Routes>
     
-      <div className="h-screen flex flex-col">
-        <CalendarHeader />
-        <div className="flex flex-1">
-          <Sidebar />
-          <Month month={currentMonth}/>
-        </div>
-      </div>
-    </React.Fragment>
   );
 }
 
---
+++ src/components/LoginPage/Account.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useContext, useEffect } from 'react';
+import '../../App.css';
+import { getMonth } from '../../util';
+import CalendarHeader from '../../components/CalendarHeader';
+import Sidebar from '../../components/Sidebar';
+import Month from '../../components/Month';
+import GlobalContext from '../../context/GlobalContext';
+import EventModel from '../../components/EventModel';
+
+const Account = () => {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex, showEventModel } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex])
+  return (
+    <React.Fragment>
+    {showEventModel && <EventModel />}
+      <div className="h-screen flex flex-col">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth}/>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Account;
