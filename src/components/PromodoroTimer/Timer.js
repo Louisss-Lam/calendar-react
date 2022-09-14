48edaf2f8eb0b4d559b632f8e77a190c0a89cf65
Louisss-Lam <louislam617@gmail.com>
2022-09-14 15:51:19 +0100

    feat: add play and pause button

--- src/components/PromodoroTimer/Timer.js
+++ src/components/PromodoroTimer/Timer.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PauseButton from './PauseButton';
+import PlayButton from './PlayButton';
 
-const red = '#f54e4e';
+const blue = 'rgba(147, 197, 253)';
 const green = '#4aec8c';
 
 const Timer = () => {
     const percentage = 66;
 
   return (
-    <div>
-        <CircularProgressbar value={percentage} text={`${percentage}%`}/>;
+    <div className='my-8'>
+        <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            background
+            backgroundPadding={6}
+            styles={buildStyles({
+                backgroundColor: blue,
+                textColor: '#fff',
+                pathColor: '#fff',
+                trailColor: "transparent"
+            })}
+        />
+        <div className='my-6 flex justify-center'>
+            <PlayButton />
+            <PauseButton />
+        </div>
     </div>
   )
 }
